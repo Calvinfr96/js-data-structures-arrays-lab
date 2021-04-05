55ec9f3da05bb8e229b0fa6423bc77c0c8b0f4d2
calvin <calvin.francis96@outlook.com>
2021-04-04 23:42:07 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name);
+}
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name);
+}
+function appendDriver(name) {
+    let newDrivers = drivers.slice(0)
+    newDrivers.push(name)
+    return newDrivers
+}
+
+function prependDriver(name) {
+    let newDrivers = drivers.slice(0)
+    newDrivers.unshift(name)
+    return newDrivers
+}
+
+function removeLastDriver() {
+    let newDrivers = drivers.slice(0)
+    newDrivers.pop(name)
+    return newDrivers
+}
+
+function removeFirstDriver() {
+    let newDrivers = drivers.slice(0)
+    newDrivers.shift(name)
+    return newDrivers
+}
